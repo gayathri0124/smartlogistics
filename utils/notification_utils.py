53d369797b42c359a34b510373da9306ea59bfb1
gayathri0124 <gayathrireddykalthireddy@gmail.com>
2025-03-05 22:40:41 -0500

    Remove all committed files

--- utils/notification_utils.py
+++
@@ -1,6 +0,0 @@
-# This file has been deprecated
-# Notification functionality has been removed from the application
-
-def dummy_function():
-    """This is a placeholder function as the notification functionality has been removed."""
-    pass
